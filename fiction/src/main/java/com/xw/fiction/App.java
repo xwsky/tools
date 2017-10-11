cf40418562abfd81fb751d9133d3de30ac598b59
sf <2213883630@qq.com>
2017-10-12 06:52:07 +0800

    分之测试

--- fiction/src/main/java/com/xw/fiction/App.java
+++ fiction/src/main/java/com/xw/fiction/App.java
@@ -80,16 +80,16 @@ public class App {
 
 	/**
 	 * 向文件写入内容
-	 * @param s
+	 * @param str
 	 */
-	private static void writeToFile(String s) {
+	private static void writeToFile(String str) {
 		File file = new File(title);
 		FileWriter fw = null;
 		BufferedWriter writer = null;
 		try {
 			fw = new FileWriter(file,true);
 			writer = new BufferedWriter(fw);
-			writer.write(s);
+			writer.write(str);
 			writer.newLine();//换行
 			writer.flush();
 		} catch (FileNotFoundException e) {
